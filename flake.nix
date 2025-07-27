a2fb9a232c1cc9f1eb4a789cbc699e19a61c0982
William Boudreault <boudreaultwilliam@gmail.com>
2025-07-27 01:09:40 -0400

    Added flake configuration and switched makefile -> CMakeLists.txt

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+    description = "A very basic http server";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }: 
+        let
+            system = "x86_64-linux";
+            pkgs = import nixpkgs { inherit system; };
+        in 
+            {
+            devShells.${system}.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } rec {
+                buildInputs = with pkgs; [
+                    cmake
+                ];
+                shellHook = ''
+                    if [ ! -d build ]; then
+                        mkdir build
+                        cd build
+                        cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+                        cd ..
+                        ln -s build/compile_commands.json ./compile_commands.json
+                    fi
+                '';
+            };
+
+            packages.${system}.default = pkgs.clangStdenv.mkDerivation {
+                pname = "httpc";
+                version = "0.1";
+                nativeBuildInputs = with pkgs; [
+                    cmake
+                ];
+                src = ./.;
+                buildPhase = ''
+                    cmake .
+                    make
+                '';
+                installPhase = ''
+                    mkdir -p $out/bin
+                    cp httpc $out/bin
+                '';
+            };
+        };
+}
